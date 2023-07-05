e3a45e866c8f5bb4b825f597506da2a79477ce3d
curLeeBrace <leeandreimain@gmail.com>
2023-07-05 19:22:57 +0800

    only learned  about manipulating an animation using css

--- src/Components/Create-Account/CreateAccount.tsx
+++ src/Components/Create-Account/CreateAccount.tsx
@@ -3,45 +3,56 @@ function CreateAccount() {
     return(
         <div id = "create-container">
             <div id = "box">
-                <div>Sign Up</div>
-            <div>
-                 <form>
-                    <div id = "form-container">
+                <div id = "box1">
+
+                    <div>Sign Up</div>
                     <div>
-                            <label>
-                                Email <br/>
-                                <input type="email"/>
-                            </label>
-                        </div>
-                        <div>
-                            <label>
-                                Username <br/>
-                                <input type="text"/>
-                            </label>
-                        </div>
-                        <div>
-                            <label>
-                                Password <br/>
-                                <input type="password"/>
-                            </label>
-                        </div>
-                        <div>
-                            <label>
-                                Re-Type Password <br/>
-                                <input type="password"/>
-                            </label>
-                        </div>
-                    
-                        <div>
-                            <button>Create</button>
-                        </div>
-                        
-                        
-                        </div>
-                </form>
-            </div>
+                        <form>
+                            <div id = "form-container">
+                            <div>
+                                    <label>
+                                        Email <br/>
+                                        <input type="email"/>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>
+                                        Username <br/>
+                                        <input type="text"/>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>
+                                        Password <br/>
+                                        <input type="password"/>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>
+                                        Re-Type Password <br/>
+                                        <input type="password"/>
+                                    </label>
+                                </div>
+                            
+                                <div>
+                                    <button>Create</button>
+                                </div>
+                                
+                                
+                                </div>
+                        </form>
+                    </div>
+
+                </div>
+                <div id = "box2">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum omnis, excepturi voluptatem quis qui enim quae sint explicabo tenetur nihil autem culpa dolor voluptate suscipit, perspiciatis impedit quibusdam, ab ipsam.</p>
+                </div>
+            
 
             </div>
+       
+
+            
 
         </div>
     );
